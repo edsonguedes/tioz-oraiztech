225ea6e59e1ba728bd1bcc260fa902f1194faeec
edsonguedes <edson@pno.com.br>
2026-06-12 09:58:39 -0300

    Replace InboxPage component with a null component

--- src/routes/admin.inbox.$leadId.tsx
+++ src/routes/admin.inbox.$leadId.tsx
@@ -1,6 +1,5 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { InboxPage } from "./admin.inbox";
 
 export const Route = createFileRoute("/admin/inbox/$leadId")({
-  component: InboxPage,
+  component: () => null,
 });
